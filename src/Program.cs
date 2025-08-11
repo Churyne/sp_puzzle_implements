e898ef881d8a760ec29a2e0fdc80b00345f4448d
MIAIONE <196308303+Churyne@users.noreply.github.com>
2025-08-11 12:03:25 +0800

    initial commt

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿var a = Enumerable.Range(2, 98)
+    .SelectMany(m => Enumerable.Range(m, 100 - m)
+        .Select(n => (m, n, s: m + n, p: m * n)));
+var (s, p) = (a.ToLookup(x => x.s), a.ToLookup(x => x.p));
+var t = p.Where(g => g.Count() > 1)
+    .Select(g => g.Where(x => s[x.s].Count() > 1 && s[x.s].All(y => p[y.p].Count() > 1)))
+    .Where(v => v.Count() == 1)
+    .SelectMany(v => v)
+    .GroupBy(x => x.s)
+    .Single(g => g.Count() == 1)
+    .Single();
+Console.WriteLine($"{t.s} = {t.m} + {t.n} , {t.p} = {t.m} * {t.n}");
